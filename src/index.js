7a4c25abc14449aeb27991ddb07a2950ecd002a5
George <losevgeorge007@gmail.com>
2021-03-16 01:53:22 +0200

    brackets done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let unique = {};
+    let existBracket ={};
+    //делаем объект с Типом скобок, куда будем записывать результат
+    bracketsConfig.forEach(el => {
+      for(let i=0; i < el.length; i++) {
+        unique[el[i]] = 0;
+      }
+    });
+
+    //теперь обходим строку, если символ есть в переданных скобках, тогда ---
+    //добавляем это символ в массив unique
+    str.split("").forEach(unit => {
+      Object.keys(unique).forEach(el => {
+        if (el === unit){
+          unique[el]++;
+        };
+      });
+    });
+    let sum = 0;
+    for (let key in unique) {
+      sum += unique[key];
+    }
+    return sum%2 === 0 ? true : false;
 }
